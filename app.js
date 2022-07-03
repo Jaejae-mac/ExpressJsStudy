0d5a8892df8192b9d55d1b67c459f9c47414082b
Youngjae <youngje3431@gmail.com>
2022-07-03 20:02:39 +0900

    Express_직원정보 조회하기(파라미터 값 받아오기)

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 
 const app = express();
-app.get('/hello', (req,res) => {
-  res.send('<h1>Hello Express</h1>');
+
+const members = require('./members');
+
+app.get('/api/members', (req,res) => {
+  res.send(members);
+});
+app.get('/api/members/:id', (req,res) => {
+  //:id -> 와 같은 양식의 값들을 Route Parameter라고 부른다.
+  // const id = req.params.id;
+  //위의 id 변수 매핑 방식과 같은 방식이다.
+  const { id } = req.params;
+  const member = members.find((m) => m.id === Number(id));
+  if(member) {
+    res.send(member);  
+  }else{
+    res.status(404).send({ message : 'There is no such member' });
+  }
+  
 });
 
 app.listen(20010, () => {
