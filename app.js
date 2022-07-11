f52882d918bf43143b57d0c781bf20d74f458047
Youngjae <youngje3431@gmail.com>
2022-07-11 22:22:14 +0900

    특정 팀만 조회하기 & 값을 URL에서 가져오는 방법

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ const app = express();
 const members = require('./members');
 
 app.get('/api/members', (req,res) => {
-  res.send(members);
+  //const team = req.query.team;//여러 파마리터 값중 team이라는 값을 가져오기위함.
+  //위의 식은 아래와 같이 표현할 수 있다.
+  const { team } = req.query;
+  console.log(team);
+  if(team) {
+    const teamMembers = members.filter((m) => m.team === team);
+    console.log(teamMembers)
+    res.send(teamMembers);
+  }else{
+    res.send(members);
+  }
 });
 app.get('/api/members/:id', (req,res) => {
   //:id -> 와 같은 양식의 값들을 Route Parameter라고 부른다.
